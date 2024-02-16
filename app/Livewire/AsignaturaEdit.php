90cc2d04eed131dd0f48049ae36cc9ffb0345b69
mmbalbas <mmbalbasgolf@gmail.com>
2024-02-16 13:42:41 +0100

    revisión de AsignaturaEdit.php

--- app/Livewire/AsignaturaEdit.php
+++ app/Livewire/AsignaturaEdit.php
@@ -1,11 +1,46 @@
 <?php
 
-namespace App\Livewire;
+namespace App\Http\Livewire;
 
+use App\Models\Asignatura;
 use Livewire\Component;
 
 class AsignaturaEdit extends Component
 {
+    public $asignaturaId;
+    public $nombre;
+    public $descripcion;
+
+    public function mount(Asignatura $asignatura)
+    {
+        $this->asignaturaId = $asignatura->id;
+        $this->nombre = $asignatura->nombre;
+        $this->descripcion = $asignatura->descripcion;
+    }
+
+    protected $rules = [
+        'nombre' => 'required',
+        'descripcion' => 'required',
+    ];
+
+    public function save()
+    {
+        $this->validate();
+
+        $asignatura = Asignatura::find($this->asignaturaId);
+        if ($asignatura) {
+            $asignatura->update([
+                'nombre' => $this->nombre,
+                'descripcion' => $this->descripcion,
+            ]);
+
+            session()->flash('message', 'Asignatura actualizada correctamente.');
+            return redirect()->to('/asignaturas');
+        } else {
+            session()->flash('error', 'Asignatura no encontrada.');
+        }
+    }
+
     public function render()
     {
         return view('livewire.asignatura-edit')->layout('layouts.app');
